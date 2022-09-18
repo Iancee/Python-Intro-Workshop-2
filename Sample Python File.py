73aa8fe39164477baa6981f676a9bda95a91e50f
Iancee <iancee@hotmail.com>
2022-09-18 14:17:23 -0700

    Add files via upload

---
+++ Sample Python File.py
@@ -0,0 +1,14 @@
+#Title of script and general purpose, date created
+import os
+#import arcpy
+
+#Set global variables
+folder = r"C:\Users\ian.conroy\Desktop\Bay Geo Classes\Python Class\Intro Class Spring 2022"
+
+#Use comments to explain what each step of the script is for
+
+#Create a variable holding the string Hello World!
+phrase = 'Hello World!'
+
+#Print the phrase
+print(phrase.upper())
